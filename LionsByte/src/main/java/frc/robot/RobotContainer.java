52c51b5c833007a526190fff8881b52d9b2d4de9
AllenF3303 <allenfriedman07@gmail.com>
2026-03-11 08:57:00 -0700

    My Nine One' git
    '
    '

--- LionsByte/src/main/java/frc/robot/RobotContainer.java
+++ LionsByte/src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,8 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -24,8 +26,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+  private final IntakeSubsystem m_intake = new IntakeSubsystem();
+  private final ShooterSubsystem m_shooter = new ShooterSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -46,6 +49,7 @@ public class RobotContainer {
                     -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
                     true),
             m_robotDrive));
+            
   }
 
   /**
@@ -59,14 +63,24 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
     
     m_driverController.leftStick().whileTrue(m_robotDrive.setXCommand());
+
+     m_driverController
+      .rightTrigger(OIConstants.kTriggerButtonThreshold)
+      .whileTrue(m_intake.runIntakeCommand());
+
+    // Left Trigger -> Run fuel intake in reverse
+    m_driverController
+      .leftTrigger(OIConstants.kTriggerButtonThreshold)
+      .whileTrue(m_intake.runExtakeCommand());
+
+    // Y Button -> Run intake and run the shooter flywheel and feeder
+    m_driverController.y().toggleOnTrue(m_shooter.runShooterCommand().alongWith(m_intake.runIntakeCommand()));
 
   }
 
@@ -75,8 +89,5 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+  
 }
